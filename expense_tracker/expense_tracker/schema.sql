37a56b00086dde3e774e82034289758f4b881078
Rifat Hossain <r_hossain@mail.fhsu.edu>
2025-11-09 14:58:40 -0500

    Add default timestamp to created_at and insert initial categories

--- expense_tracker/expense_tracker/schema.sql
+++ expense_tracker/expense_tracker/schema.sql
@@ -21,8 +21,21 @@ CREATE TABLE transactions (
     type TEXT CHECK(type IN ('income','expense')) NOT NULL,
     category_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
-    created_at TEXT NOT NULL,
+    created_at TEXT NOT NULL DEFAULT (datetime('now')),
     note TEXT,
     FOREIGN KEY (category_id) REFERENCES category (id),
     FOREIGN KEY (user_id) REFERENCES user (id)
 );
+
+INSERT INTO category (name, user_id) VALUES
+('Housing & Utilities', 1),
+('Transportation', 1),
+('Food & Dining', 1),
+('Health & Wellness', 1),
+('Personal & Lifestyle', 1),
+('Education & Development', 1),
+('Finance & Debt', 1),
+('Work & Professional', 1),
+('Travel & Vacation', 1),
+('Miscellaneous / Other', 1);
+
